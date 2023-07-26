4f61bf71be20811b6645a001ea81df0534908602
Giovanne Feitosa <giovanneafonso@gmail.com>
2023-07-26 14:20:13 -0300

    add tailwind classes

--- packages/shared-ui/src/pages/TailwindGeneratorPage.tsx
+++ packages/shared-ui/src/pages/TailwindGeneratorPage.tsx
@@ -201,6 +201,83 @@ function AllTailwindColors() {
     hover:bg-fuchsia-50 hover:bg-fuchsia-100 hover:bg-fuchsia-200 hover:bg-fuchsia-300 hover:bg-fuchsia-400 hover:bg-fuchsia-500 hover:bg-fuchsia-600 hover:bg-fuchsia-700 hover:bg-fuchsia-800 hover:bg-fuchsia-900 hover:bg-fuchsia-950
     hover:bg-pink-50 hover:bg-pink-100 hover:bg-pink-200 hover:bg-pink-300 hover:bg-pink-400 hover:bg-pink-500 hover:bg-pink-600 hover:bg-pink-700 hover:bg-pink-800 hover:bg-pink-900 hover:bg-pink-950
     hover:bg-rose-50 hover:bg-rose-100 hover:bg-rose-200 hover:bg-rose-300 hover:bg-rose-400 hover:bg-rose-500 hover:bg-rose-600 hover:bg-rose-700 hover:bg-rose-800 hover:bg-rose-900 hover:bg-rose-950
+
+    text-slate-50 text-slate-100 text-slate-200 text-slate-300 text-slate-400 text-slate-500 text-slate-600 text-slate-700 text-slate-800 text-slate-900 text-slate-950
+    text-gray-50 text-gray-100 text-gray-200 text-gray-300 text-gray-400 text-gray-500 text-gray-600 text-gray-700 text-gray-800 text-gray-900 text-gray-950
+    text-zinc-50 text-zinc-100 text-zinc-200 text-zinc-300 text-zinc-400 text-zinc-500 text-zinc-600 text-zinc-700 text-zinc-800 text-zinc-900 text-zinc-950
+    text-neutral-50 text-neutral-100 text-neutral-200 text-neutral-300 text-neutral-400 text-neutral-500 text-neutral-600 text-neutral-700 text-neutral-800 text-neutral-900 text-neutral-950
+    text-stone-50 text-stone-100 text-stone-200 text-stone-300 text-stone-400 text-stone-500 text-stone-600 text-stone-700 text-stone-800 text-stone-900 text-stone-950
+    text-red-50 text-red-100 text-red-200 text-red-300 text-red-400 text-red-500 text-red-600 text-red-700 text-red-800 text-red-900 text-red-950
+    text-orange-50 text-orange-100 text-orange-200 text-orange-300 text-orange-400 text-orange-500 text-orange-600 text-orange-700 text-orange-800 text-orange-900 text-orange-950
+    text-amber-50 text-amber-100 text-amber-200 text-amber-300 text-amber-400 text-amber-500 text-amber-600 text-amber-700 text-amber-800 text-amber-900 text-amber-950
+    text-yellow-50 text-yellow-100 text-yellow-200 text-yellow-300 text-yellow-400 text-yellow-500 text-yellow-600 text-yellow-700 text-yellow-800 text-yellow-900 text-yellow-950
+    text-lime-50 text-lime-100 text-lime-200 text-lime-300 text-lime-400 text-lime-500 text-lime-600 text-lime-700 text-lime-800 text-lime-900 text-lime-950
+    text-green-50 text-green-100 text-green-200 text-green-300 text-green-400 text-green-500 text-green-600 text-green-700 text-green-800 text-green-900 text-green-950
+    text-emerald-50 text-emerald-100 text-emerald-200 text-emerald-300 text-emerald-400 text-emerald-500 text-emerald-600 text-emerald-700 text-emerald-800 text-emerald-900 text-emerald-950
+    text-teal-50 text-teal-100 text-teal-200 text-teal-300 text-teal-400 text-teal-500 text-teal-600 text-teal-700 text-teal-800 text-teal-900 text-teal-950
+    text-cyan-50 text-cyan-100 text-cyan-200 text-cyan-300 text-cyan-400 text-cyan-500 text-cyan-600 text-cyan-700 text-cyan-800 text-cyan-900 text-cyan-950
+    text-sky-50 text-sky-100 text-sky-200 text-sky-300 text-sky-400 text-sky-500 text-sky-600 text-sky-700 text-sky-800 text-sky-900 text-sky-950
+    text-blue-50 text-blue-100 text-blue-200 text-blue-300 text-blue-400 text-blue-500 text-blue-600 text-blue-700 text-blue-800 text-blue-900 text-blue-950
+    text-indigo-50 text-indigo-100 text-indigo-200 text-indigo-300 text-indigo-400 text-indigo-500 text-indigo-600 text-indigo-700 text-indigo-800 text-indigo-900 text-indigo-950
+    text-violet-50 text-violet-100 text-violet-200 text-violet-300 text-violet-400 text-violet-500 text-violet-600 text-violet-700 text-violet-800 text-violet-900 text-violet-950
+    text-purple-50 text-purple-100 text-purple-200 text-purple-300 text-purple-400 text-purple-500 text-purple-600 text-purple-700 text-purple-800 text-purple-900 text-purple-950
+    text-fuchsia-50 text-fuchsia-100 text-fuchsia-200 text-fuchsia-300 text-fuchsia-400 text-fuchsia-500 text-fuchsia-600 text-fuchsia-700 text-fuchsia-800 text-fuchsia-900 text-fuchsia-950
+    text-pink-50 text-pink-100 text-pink-200 text-pink-300 text-pink-400 text-pink-500 text-pink-600 text-pink-700 text-pink-800 text-pink-900 text-pink-950
+    text-rose-50 text-rose-100 text-rose-200 text-rose-300 text-rose-400 text-rose-500 text-rose-600 text-rose-700 text-rose-800 text-rose-900 text-rose-950
+
+    hover:text-slate-50 hover:text-slate-100 hover:text-slate-200 hover:text-slate-300 hover:text-slate-400 hover:text-slate-500 hover:text-slate-600 hover:text-slate-700 hover:text-slate-800 hover:text-slate-900 hover:text-slate-950
+    hover:text-gray-50 hover:text-gray-100 hover:text-gray-200 hover:text-gray-300 hover:text-gray-400 hover:text-gray-500 hover:text-gray-600 hover:text-gray-700 hover:text-gray-800 hover:text-gray-900 hover:text-gray-950
+    hover:text-zinc-50 hover:text-zinc-100 hover:text-zinc-200 hover:text-zinc-300 hover:text-zinc-400 hover:text-zinc-500 hover:text-zinc-600 hover:text-zinc-700 hover:text-zinc-800 hover:text-zinc-900 hover:text-zinc-950
+    hover:text-neutral-50 hover:text-neutral-100 hover:text-neutral-200 hover:text-neutral-300 hover:text-neutral-400 hover:text-neutral-500 hover:text-neutral-600 hover:text-neutral-700 hover:text-neutral-800 hover:text-neutral-900 hover:text-neutral-950
+    hover:text-stone-50 hover:text-stone-100 hover:text-stone-200 hover:text-stone-300 hover:text-stone-400 hover:text-stone-500 hover:text-stone-600 hover:text-stone-700 hover:text-stone-800 hover:text-stone-900 hover:text-stone-950
+    hover:text-red-50 hover:text-red-100 hover:text-red-200 hover:text-red-300 hover:text-red-400 hover:text-red-500 hover:text-red-600 hover:text-red-700 hover:text-red-800 hover:text-red-900 hover:text-red-950
+    hover:text-orange-50 hover:text-orange-100 hover:text-orange-200 hover:text-orange-300 hover:text-orange-400 hover:text-orange-500 hover:text-orange-600 hover:text-orange-700 hover:text-orange-800 hover:text-orange-900 hover:text-orange-950
+    hover:text-amber-50 hover:text-amber-100 hover:text-amber-200 hover:text-amber-300 hover:text-amber-400 hover:text-amber-500 hover:text-amber-600 hover:text-amber-700 hover:text-amber-800 hover:text-amber-900 hover:text-amber-950
+    hover:text-yellow-50 hover:text-yellow-100 hover:text-yellow-200 hover:text-yellow-300 hover:text-yellow-400 hover:text-yellow-500 hover:text-yellow-600 hover:text-yellow-700 hover:text-yellow-800 hover:text-yellow-900 hover:text-yellow-950
+    hover:text-lime-50 hover:text-lime-100 hover:text-lime-200 hover:text-lime-300 hover:text-lime-400 hover:text-lime-500 hover:text-lime-600 hover:text-lime-700 hover:text-lime-800 hover:text-lime-900 hover:text-lime-950
+    hover:text-green-50 hover:text-green-100 hover:text-green-200 hover:text-green-300 hover:text-green-400 hover:text-green-500 hover:text-green-600 hover:text-green-700 hover:text-green-800 hover:text-green-900 hover:text-green-950
+    hover:text-emerald-50 hover:text-emerald-100 hover:text-emerald-200 hover:text-emerald-300 hover:text-emerald-400 hover:text-emerald-500 hover:text-emerald-600 hover:text-emerald-700 hover:text-emerald-800 hover:text-emerald-900 hover:text-emerald-950
+    hover:text-teal-50 hover:text-teal-100 hover:text-teal-200 hover:text-teal-300 hover:text-teal-400 hover:text-teal-500 hover:text-teal-600 hover:text-teal-700 hover:text-teal-800 hover:text-teal-900 hover:text-teal-950
+    hover:text-cyan-50 hover:text-cyan-100 hover:text-cyan-200 hover:text-cyan-300 hover:text-cyan-400 hover:text-cyan-500 hover:text-cyan-600 hover:text-cyan-700 hover:text-cyan-800 hover:text-cyan-900 hover:text-cyan-950
+    hover:text-sky-50 hover:text-sky-100 hover:text-sky-200 hover:text-sky-300 hover:text-sky-400 hover:text-sky-500 hover:text-sky-600 hover:text-sky-700 hover:text-sky-800 hover:text-sky-900 hover:text-sky-950
+    hover:text-blue-50 hover:text-blue-100 hover:text-blue-200 hover:text-blue-300 hover:text-blue-400 hover:text-blue-500 hover:text-blue-600 hover:text-blue-700 hover:text-blue-800 hover:text-blue-900 hover:text-blue-950
+    hover:text-indigo-50 hover:text-indigo-100 hover:text-indigo-200 hover:text-indigo-300 hover:text-indigo-400 hover:text-indigo-500 hover:text-indigo-600 hover:text-indigo-700 hover:text-indigo-800 hover:text-indigo-900 hover:text-indigo-950
+    hover:text-violet-50 hover:text-violet-100 hover:text-violet-200 hover:text-violet-300 hover:text-violet-400 hover:text-violet-500 hover:text-violet-600 hover:text-violet-700 hover:text-violet-800 hover:text-violet-900 hover:text-violet-950
+    hover:text-purple-50 hover:text-purple-100 hover:text-purple-200 hover:text-purple-300 hover:text-purple-400 hover:text-purple-500 hover:text-purple-600 hover:text-purple-700 hover:text-purple-800 hover:text-purple-900 hover:text-purple-950
+    hover:text-fuchsia-50 hover:text-fuchsia-100 hover:text-fuchsia-200 hover:text-fuchsia-300 hover:text-fuchsia-400 hover:text-fuchsia-500 hover:text-fuchsia-600 hover:text-fuchsia-700 hover:text-fuchsia-800 hover:text-fuchsia-900 hover:text-fuchsia-950
+    hover:text-pink-50 hover:text-pink-100 hover:text-pink-200 hover:text-pink-300 hover:text-pink-400 hover:text-pink-500 hover:text-pink-600 hover:text-pink-700 hover:text-pink-800 hover:text-pink-900 hover:text-pink-950
+    hover:text-rose-50 hover:text-rose-100 hover:text-rose-200 hover:text-rose-300 hover:text-rose-400 hover:text-rose-500 hover:text-rose-600 hover:text-rose-700 hover:text-rose-800 hover:text-rose-900 hover:text-rose-950
+
+    fill-slate-50 fill-slate-100 fill-slate-200 fill-slate-300 fill-slate-400 fill-slate-500 fill-slate-600 fill-slate-700 fill-slate-800 fill-slate-900 fill-slate-950
+    fill-gray-50 fill-gray-100 fill-gray-200 fill-gray-300 fill-gray-400 fill-gray-500 fill-gray-600 fill-gray-700 fill-gray-800 fill-gray-900 fill-gray-950
+    fill-zinc-50 fill-zinc-100 fill-zinc-200 fill-zinc-300 fill-zinc-400 fill-zinc-500 fill-zinc-600 fill-zinc-700 fill-zinc-800 fill-zinc-900 fill-zinc-950
+    fill-neutral-50 fill-neutral-100 fill-neutral-200 fill-neutral-300 fill-neutral-400 fill-neutral-500 fill-neutral-600 fill-neutral-700 fill-neutral-800 fill-neutral-900 fill-neutral-950
+    fill-stone-50 fill-stone-100 fill-stone-200 fill-stone-300 fill-stone-400 fill-stone-500 fill-stone-600 fill-stone-700 fill-stone-800 fill-stone-900 fill-stone-950
+    fill-red-50 fill-red-100 fill-red-200 fill-red-300 fill-red-400 fill-red-500 fill-red-600 fill-red-700 fill-red-800 fill-red-900 fill-red-950
+    fill-orange-50 fill-orange-100 fill-orange-200 fill-orange-300 fill-orange-400 fill-orange-500 fill-orange-600 fill-orange-700 fill-orange-800 fill-orange-900 fill-orange-950
+    fill-amber-50 fill-amber-100 fill-amber-200 fill-amber-300 fill-amber-400 fill-amber-500 fill-amber-600 fill-amber-700 fill-amber-800 fill-amber-900 fill-amber-950
+    fill-yellow-50 fill-yellow-100 fill-yellow-200 fill-yellow-300 fill-yellow-400 fill-yellow-500 fill-yellow-600 fill-yellow-700 fill-yellow-800 fill-yellow-900 fill-yellow-950
+    fill-lime-50 fill-lime-100 fill-lime-200 fill-lime-300 fill-lime-400 fill-lime-500 fill-lime-600 fill-lime-700 fill-lime-800 fill-lime-900 fill-lime-950
+    fill-green-50 fill-green-100 fill-green-200 fill-green-300 fill-green-400 fill-green-500 fill-green-600 fill-green-700 fill-green-800 fill-green-900 fill-green-950
+    fill-emerald-50 fill-emerald-100 fill-emerald-200 fill-emerald-300 fill-emerald-400 fill-emerald-500 fill-emerald-600 fill-emerald-700 fill-emerald-800 fill-emerald-900 fill-emerald-950
+    fill-teal-50 fill-teal-100 fill-teal-200 fill-teal-300 fill-teal-400 fill-teal-500 fill-teal-600 fill-teal-700 fill-teal-800 fill-teal-900 fill-teal-950
+    fill-cyan-50 fill-cyan-100 fill-cyan-200 fill-cyan-300 fill-cyan-400 fill-cyan-500 fill-cyan-600 fill-cyan-700 fill-cyan-800 fill-cyan-900 fill-cyan-950
+    fill-sky-50 fill-sky-100 fill-sky-200 fill-sky-300 fill-sky-400 fill-sky-500 fill-sky-600 fill-sky-700 fill-sky-800 fill-sky-900 fill-sky-950
+    fill-blue-50 fill-blue-100 fill-blue-200 fill-blue-300 fill-blue-400 fill-blue-500 fill-blue-600 fill-blue-700 fill-blue-800 fill-blue-900 fill-blue-950
+    fill-indigo-50 fill-indigo-100 fill-indigo-200 fill-indigo-300 fill-indigo-400 fill-indigo-500 fill-indigo-600 fill-indigo-700 fill-indigo-800 fill-indigo-900 fill-indigo-950
+    fill-violet-50 fill-violet-100 fill-violet-200 fill-violet-300 fill-violet-400 fill-violet-500 fill-violet-600 fill-violet-700 fill-violet-800 fill-violet-900 fill-violet-950
+    fill-purple-50 fill-purple-100 fill-purple-200 fill-purple-300 fill-purple-400 fill-purple-500 fill-purple-600 fill-purple-700 fill-purple-800 fill-purple-900 fill-purple-950
+    fill-fuchsia-50 fill-fuchsia-100 fill-fuchsia-200 fill-fuchsia-300 fill-fuchsia-400 fill-fuchsia-500 fill-fuchsia-600 fill-fuchsia-700 fill-fuchsia-800 fill-fuchsia-900 fill-fuchsia-950
+    fill-pink-50 fill-pink-100 fill-pink-200 fill-pink-300 fill-pink-400 fill-pink-500 fill-pink-600 fill-pink-700 fill-pink-800 fill-pink-900 fill-pink-950
+    fill-rose-50 fill-rose-100 fill-rose-200 fill-rose-300 fill-rose-400 fill-rose-500 fill-rose-600 fill-rose-700 fill-rose-800 fill-rose-900 fill-rose-950
+
+    animate-spin
+    animate-ping
+    animate-pulse
+    animate-bounce
+    animate-none
+    
+    sr-only
   `;
   return <div className={className} style={{display: 'none'}}></div>;
 }
