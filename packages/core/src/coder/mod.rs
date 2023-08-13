dce883975520e526d8c2136cc4921d0a607fb70c
Giovanne Feitosa <giovanneafonso@gmail.com>
2023-08-13 13:38:45 -0300

    add open

--- packages/core/src/coder/mod.rs
+++ packages/core/src/coder/mod.rs
@@ -34,11 +34,16 @@ pub fn run_actions(project_dir: &str, actions: &Vec<MissionAction>) -> Result<()
 }
 
 pub fn open_file_in_editor(project_dir: &str, relative_path: &str) -> Result<()> {
-    // let file_path = format!("{}/{}", project_dir, relative_path);
-    // let file_path = std::path::Path::new(&file_path);
-    // open file in editor
-    run_shell_command("code .", project_dir);
-    std::thread::sleep(std::time::Duration::from_secs(1));
-    run_shell_command(&format!("code {}", relative_path), project_dir);
+    if cfg!(windows) {
+        open::that(format!("{}/{}", project_dir, relative_path))?;
+    } else if cfg!(unix) {
+        // this works in wsl
+        // let file_path = format!("{}/{}", project_dir, relative_path);
+        // let file_path = std::path::Path::new(&file_path);
+        // open file in editor
+        run_shell_command("code .", project_dir);
+        std::thread::sleep(std::time::Duration::from_secs(1));
+        run_shell_command(&format!("code {}", relative_path), project_dir);
+    }
     Ok(())
 }
